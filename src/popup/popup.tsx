e24dfbf1c8dbbb2a08066957e012cba1dd3fd1b2
Amardeep Ganguly <amardeepganguly@Amardeeps-MacBook-Air.local>
2024-04-09 01:15:31 +0530

    setup-manifest-file

---
+++ src/popup/popup.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const test = <p>Hello</p>;
+const root = document.createElement("div");
+document.body.appendChild(root);
+ReactDOM.render(test, root);
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,41 @@
-const path = require('path');
-module.exports ={
-    mode:"development",
-    entry:'./src/test.tsx',
-    module:{
-        rules:[
-            {
-                use:'ts-loader',
-                test:/\.tsx?$/,
-                exclude:/node_modules/
-            }
-        ]
-    },
-    resolve:{
-        extensions:['.tsx','.ts','.js']
-    },
-    output:{
-        filename:'index.js',
-        path:path.resolve(__dirname,'dist')
-    }
-}
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "development",
+  devtool: "cheap-module-source-map",
+  entry: {
+    popup: path.resolve("src/popup/popup.tsx"),
+  },
+  module: {
+    rules: [
+      {
+        use: "ts-loader",
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve("src/manifest.json"),
+          to: path.resolve("dist"),
+        },
+      ],
+    }),
+    new HtmlPlugin({
+      title: "React Extensio",
+      filename: "popup.html",
+      chunks: ["popup"],
+    }),
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
