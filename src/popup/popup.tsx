6b5a4295ec449855cbc5b2a8e4fe46658e94e39e
Amardeep Ganguly <amardeepganguly@Amardeeps-MacBook-Air.local>
2024-05-24 00:24:09 +0530

    add-storage

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,24 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "./popup.css";
 import WeatherCard from "./WeatherCard";
 import { Box } from "@mui/material";
 import { CityInput } from "../common/Utils";
+import { getStoredCities, setStoredCities } from "../utils/storage";
 
 const App: React.FC<{}> = () => {
   const [cities, setCities] = useState<string[]>([]);
   const [cityQuery, setCityQuery] = useState<string>("");
   // This function will add new city on add
   const onAdd = (city: string) => {
-    setCities([...cities, city]);
-    // reset query
-    setCityQuery("");
+    const newCities = [...cities, city];
+    setStoredCities(newCities).then(() => {
+      setCities(newCities);
+      // reset query
+      setCityQuery("");
+    });
   };
+  // This function will delete the city
   const onDelete = (index: number) => {
-    // This function will delete the city
     cities.splice(index, 1);
-    setCities([...cities]);
+    const newCities = [...cities];
+    setStoredCities(cities).then(() => {
+      setCities(newCities);
+    });
   };
+  // on initial mount get from local storage
+  useEffect(() => {
+    getStoredCities().then((res) => {
+      setCities(res);
+    });
+  }, []);
   return (
     <Box px="10px" py="20px">
       <CityInput
